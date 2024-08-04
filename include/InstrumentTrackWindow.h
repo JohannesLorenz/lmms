9584b57ba4d1f2404051c9e9d93d69139cfa47d3
Johannes Lorenz <j.git@lorenz-ho.me>
2024-08-04 21:44:42 +0200

    Implement Lv2 UI
    http://lv2plug.in/ns/extensions/ui
    
    Size issues also fixed (but SlicerT is not happy as we removed
    `InstrumentTrackWindow::resizeEvent`...
    
    Co-authored-by: Tres Finocchiaro <tres.finocchiaro@gmail.com>

--- include/InstrumentTrackWindow.h
+++ include/InstrumentTrackWindow.h
@@ -68,8 +68,6 @@ public:
 	InstrumentTrackWindow( InstrumentTrackView * _tv );
 	~InstrumentTrackWindow() override;
 
-	void resizeEvent(QResizeEvent* event) override;
-
 
 	// parent for all internal tab-widgets
 	TabWidget * tabWidgetParent()
